92ca10b7f93933fcf607d7294bb73a6f1694811b
Sajit Manandhar <manandharsajit@gmail.com>
2024-02-24 09:38:11 +0545

    Integer Wrapper class

--- Git.java
+++ Git.java
@@ -1,10 +1,13 @@
 public class Git {
 
-   int a = 120;
+    int a = 120;
+
 
     public static void main(String args[]){
 
-
+        Integer a = new Integer(10);
+        int b = a.intValue();
+        System.out.println(b);
         Git instance = new Git();
         System.out.println(instance.a);
 
