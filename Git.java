cf15de896f17f613c61e3352e1f07fa08769873b
Sajit Manandhar <manandharsajit@gmail.com>
2024-02-24 09:19:33 +0545

    Changes

--- Git.java
+++ Git.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Git {
+public class Git {
+
+   static int a = 120;
+
+    public static void main(String args[]){
+
+
+        System.out.println(a);
+
+    }
+
 }
