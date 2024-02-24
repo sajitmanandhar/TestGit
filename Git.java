f1cb345f3e9ca466e28c2aef7eb38615b70d45c7
Sajit Manandhar <manandharsajit@gmail.com>
2024-02-24 09:48:08 +0545

    one commi

--- Git.java
+++ Git.java
@@ -6,6 +6,10 @@ public class Git {
     public static void main(String args[]){
 
         Integer a = new Integer(10);
+
+        a=10;
+        b=79;
+        c=30;
         int b = a.intValue();
         System.out.println(b);
         Git instance = new Git();
